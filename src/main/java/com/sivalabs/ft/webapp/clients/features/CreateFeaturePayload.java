032962d60b70d331dfb842888e32bc33e55eb20e
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2025-04-15 17:26:29 +0530

    Upgraded Spring Boot and other library versions

--- src/main/java/com/sivalabs/ft/webapp/clients/features/CreateFeaturePayload.java
+++ src/main/java/com/sivalabs/ft/webapp/clients/features/CreateFeaturePayload.java
@@ -6,9 +6,7 @@ import jakarta.validation.constraints.Size;
 public record CreateFeaturePayload(
         @NotEmpty(message = "Product code is required") String productCode,
         @NotEmpty(message = "Release code is required") String releaseCode,
-        @NotEmpty(message = "Code is required") @Size(max = 50, message = "Code cannot exceed 50 characters")
-                String code,
-        @NotEmpty(message = "Title is required") @Size(max = 500, message = "Title cannot exceed 500 characters")
-                String title,
+        @NotEmpty(message = "Code is required") @Size(max = 50, message = "Code cannot exceed 50 characters") String code,
+        @NotEmpty(message = "Title is required") @Size(max = 500, message = "Title cannot exceed 500 characters") String title,
         String description,
         String assignedTo) {}
